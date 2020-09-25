432675224dabb564860585d9524bc23ac57b2edc
jazzmam <ruta.kalytyte@gmail.com>
2020-09-25 22:14:21 +0200

    Update prices_scraper.py

--- prices_scraper.py
+++ prices_scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import smtplib
 
 
-#URL = 'https://www.aruodas.lt/namai-vilniuje-didziuosiuose-gulbinuose-moletu-pl-3-kambariu-kotedzas-uzmiestyje-uz-studijos-2-1304983/'
 URL = 'https://www.aruodas.lt/gyvenamieji-namai/vilniaus-rajone/?FBuildingType=box&FAreaOverAllMin=55&FAreaOverAllMax=150&FPriceMax=100000&FPriceMin=55000'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
@@ -14,11 +13,28 @@ def checkPrice():
 
 	soup = BeautifulSoup(page.content, 'html.parser')
 
-	price = soup.find(class_= "list-item-price").getText()
-	price_elements = soup.find_all(class_= "list-item-price")
 
-	for price in price_elements:
-		print(price.getText())
+	all_lists = soup.find_all(class_= "list-row")
+	price_blocks = soup.find_all(class_= "price")
+
+
+	for list in all_lists:
+		address = list.select('.list-adress h3 a')
+		#post_url = address_block.find('a').get('href')
+
+		building_area = list.find(class_= "list-AreaOverall")
+		land_area = list.find(class_= "list-AreaLot")
+		building_state = list.find(class_= "list-HouseStates")
+
+		print(address)
+		print("AAAA")
+
+
+	for list in price_blocks:
+		price = list.find(class_= "list-item-price")
+		price_for_sq_meter = list.find(class_= "price-pm")
+
+		#print(price.getText() + " " + price_for_sq_meter.getText())
 
 
 """
